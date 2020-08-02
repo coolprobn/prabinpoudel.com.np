2e851d8c1efc2d89f7a22a6452151d00cce28091
Prabin Poudel <probnpoudel@gmail.com>
2020-08-02 10:51:54 +0545

    Feature/11 Testimonial section
    - Add testimonials from clients
    - Create carousel for testimonials
    - Enable dark mode

---
+++ src/components/carousel.js
@@ -0,0 +1,96 @@
+import React, { useState } from 'react';
+
+import style from '../styles/testimonials.module.css';
+import carouselStyle from '../styles/carousel.module.css';
+
+const History = ({ activeIndex, items, changeSlide }) => {
+  const itemList = items.map((el, index) => {
+    const name = index == activeIndex ? carouselStyle.active : '';
+
+    return (
+      <li key={index}>
+        <button
+          className={name}
+          onClick={() => changeSlide(activeIndex, index)}
+        />
+      </li>
+    );
+  });
+
+  return <ul>{itemList}</ul>;
+};
+
+const Carousel = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const goToHistoryClick = (curIndex, index) => {
+    setActiveIndex(index);
+  };
+
+  const content = items.map((item, index) => {
+    const {
+      uid,
+      name,
+      company,
+      post,
+      feedback,
+      imageUrl,
+      platform,
+      platformUrl,
+    } = item;
+    const activeItemClass =
+      index === activeIndex ? carouselStyle.activeCarousel : '';
+
+    return (
+      <div
+        key={uid}
+        className={`custom-block notice ${carouselStyle.container} ${activeItemClass}`}
+      >
+        <a
+          href={platformUrl}
+          className={`btn ${style.platformButton}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {platform}
+        </a>
+
+        <div className={style.feedback}>
+          <q>{feedback}</q>
+        </div>
+
+        <div className={style.clientContainer}>
+          <img
+            src={imageUrl}
+            alt="client-image"
+            className={style.clientImage}
+          />
+
+          <div className={style.clientInfo}>
+            <div className={style.clientName}>{name}</div>
+
+            <div className={style.clientCompanyInfo}>
+              <span className={style.clientPost}>{`${post}, `}</span>
+              <span>{company}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div className={carouselStyle.carousel}>
+      {content}
+      <div className={carouselStyle.history}>
+        <History
+          activeIndex={activeIndex}
+          items={items}
+          changeSlide={goToHistoryClick}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
---
+++ src/data/testimonial.js
@@ -0,0 +1,44 @@
+import ReneldyImage from '../images/clients/reneldy.webp';
+import EdwardImage from '../images/clients/edward.webp';
+import AntonImage from '../images/clients/anton.webp';
+
+const testimonials = [
+  {
+    uid: 1,
+    name: 'Reneldy Senat',
+    company: 'Idoleyes Interactive',
+    post: 'Owner',
+    feedback:
+      'I hired Prabin to build our mobile app and he did a great job! He was able to get it built and released to both Apple and Google Play app stores with great quality. Prabin has been the ideal partner; flexible, reliable, and very responsive. He also has great time management skills and always comes to the table with ideas and suggestions to help make the project run smoother. Highly recommended!',
+    imageUrl:
+      ReneldyImage,
+    platform: 'Truemark',
+    platformUrl: 'https://truemark.com.np/hire/resources/',
+  },
+  {
+    uid: 2,
+    name: 'Anton Hughes',
+    company: 'Price Insight',
+    post: 'Owner',
+    feedback:
+      'Prabin is really easy to work with, and has a good understanding of react-native and javascript. I hope to work with him again one day.',
+    imageUrl:
+      AntonImage,
+    platform: 'Upwork',
+    platformUrl: 'https://www.upwork.com/freelancers/~0184b506a4486b8f86',
+  },
+  {
+    uid: 2,
+    name: 'Edward Irvin',
+    company: 'Eddie Makes',
+    post: 'Owner',
+    feedback:
+      'Very thankful for your work and patience to understand! I really appreciate it',
+    imageUrl:
+      EdwardImage,
+    platform: 'Upwork',
+    platformUrl: 'https://www.upwork.com/freelancers/~0184b506a4486b8f86',
+  },
+];
+
+export default testimonials;
