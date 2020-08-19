4b5efe5705ff2ff28db24f08b78f36fe1f2b4896
Prabin Poudel <probnpoudel@gmail.com>
2020-08-19 18:56:57 +0545

    Remove name image from og property

--- src/components/seo/open-graph.js
+++ src/components/seo/open-graph.js
@@ -20,7 +20,7 @@ const OpenGraph = ({
     <meta property="og:type" content={type} />
     <meta property="og:title" content={title} />
     <meta property="og:description" content={description} />
-    <meta name="image" property="og:image" content={image} />
+    <meta property="og:image" content={image} />
     {width && <meta property="og:image:width" content={width} />}
     {height && <meta property="og:image:height" content={height} />}
   </Helmet>
