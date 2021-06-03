b1481883f057ea07ec830c6ddb44b5e324e3af06
Prabin Poudel <probnpoudel@gmail.com>
2021-06-03 23:09:19 +0545

    Update packages to wanted version

--- gatsby-config.js
+++ gatsby-config.js
@@ -400,11 +400,5 @@ module.exports = {
         icon: `src${site.favicon}`,
       },
     },
-    {
-      resolve: `gatsby-plugin-google-adsense`,
-      options: {
-        publisherId: process.env.GOOGLE_AD_SENSE_PUBLISHER_ID,
-      },
-    },
   ],
 };
